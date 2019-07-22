13594af90c94d35dce60ac843a7239e7ccba1d9a
pyronasaur <pyronnicholas@gmail.com>
2019-07-22 12:35:44 -0400

    Added welcome wagon text to main, and cleaned up trip_saver code with better comments

--- assets/javascript/trip_saver.js
+++ assets/javascript/trip_saver.js
@@ -1,31 +1,32 @@
   $(document).ready(function(){
-  console.log("will anything log?");
 
-  firebase.initializeApp(firebaseConfig);
+    //initialize and create a db reference variable.
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
 
-  // Create a variable to reference the database.
-  var database = firebase.database();
+    // fill dropdown with existing values
+    initDropdown();
+    
+    // Initial Values
+    var travelerName = sessionStorage.getItem("user");
+    var startDate = "";
+    var endDate = "";
+    var location = "";
+    var tripName = "";
 
-  // fill dropdown with existing values
-  initDropdown();
+    //set welcome banner
+    $("#welcome-banner").text("Welcome " + travelerName + "!");
 
-  // Initial Values
-  var travelerName = sessionStorage.getItem("user");
-  var startDate = "";
-  var endDate = "";
-  var location = "";
-  var tripName = "";
+    // Capture dropdown Button Click
+    $(document).on("click", ".saved-trip", function(event) {
+      event.preventDefault();
+      var trippy = $(this).text();
+      console.log(trippy);
+      loadSavedTrip(trippy);
 
-  // Capture dropdown Button Click
-  $(document).on("click", ".saved-trip", function(event) {
-    event.preventDefault();
-    var trippy = $(this).text();
-    console.log(trippy);
-    loadSavedTrip(trippy);
+    });
 
-  });
-
-    // Capture Button Click
+    // Capture submit Button Click and save entries to db
     $("#submit-btn").on("click", function(event) {
         event.preventDefault();
 
@@ -41,7 +42,7 @@
         console.log(location);
         console.log(tripName);
 
-        // Code for handling the push
+        // Code for handling the db data insert
         database.ref(travelerName+"/"+tripName).set({
             startDate: startDate,
             endDate: endDate,
@@ -56,46 +57,47 @@
         existingTrip.attr("type", "button");
         $("#dropdownMenu").append(existingTrip);
             
-      });
-
-  //function to populate dropdown on page load
-  //TODO: add API call to button selection
-  function initDropdown()
-  {
-    var user = sessionStorage.getItem("newUser");
-    var travelerName = sessionStorage.getItem("user");
-    if(user)
-    {
-      database.ref(travelerName).orderByValue().once('value')
-        .then(function(snapshot)
-        {                
-            console.log(snapshot.val());
-            snapshot.forEach(function(childSnapshot)
-            {
-              var existingTrip = $("<button>");
-              existingTrip.html(childSnapshot.key);
-              existingTrip.addClass("dropdown-item saved-trip");
-              existingTrip.addClass(childSnapshot.val().location);
-              existingTrip.attr("type", "button");
-              $("#dropdownMenu").append(existingTrip);
-            })
-        })
-    }
-  }
-
-  function loadSavedTrip(tripName)
-  {
-    database.ref(travelerName + "/" + tripName).orderByValue().once('value')
-      .then(function(snapshot)
-      {
-        var data = snapshot.val();                        
-        console.log(data);
-
-        $("#location-input").val(data.location);
-        $("#departure-input").val(data.startDate);
-        $("#return-input").val(data.endDate);          
-        $("#tripName-input").val(tripName);
     });
-  }
+
+    //function to populate dropdown on page load
+    function initDropdown()
+    {
+      var user = sessionStorage.getItem("newUser");
+      var travelerName = sessionStorage.getItem("user");
+      if(user)
+      {
+        database.ref(travelerName).orderByValue().once('value')
+          .then(function(snapshot)
+          {                
+              console.log(snapshot.val());
+              snapshot.forEach(function(childSnapshot)
+              {
+                var existingTrip = $("<button>");
+                existingTrip.html(childSnapshot.key);
+                existingTrip.addClass("dropdown-item saved-trip");
+                existingTrip.addClass(childSnapshot.val().location);
+                existingTrip.attr("type", "button");
+                $("#dropdownMenu").append(existingTrip);
+              })
+          })
+      }
+    }
+
+    //load saved trip values for selected dropdown item into input fields
+    //TODO: add API call to button selection
+    function loadSavedTrip(tripName)
+    {
+      database.ref(travelerName + "/" + tripName).orderByValue().once('value')
+        .then(function(snapshot)
+        {
+          var data = snapshot.val();                        
+          console.log(data);
+
+          $("#location-input").val(data.location);
+          $("#departure-input").val(data.startDate);
+          $("#return-input").val(data.endDate);          
+          $("#tripName-input").val(tripName);
+      });
+    }
 
 });
