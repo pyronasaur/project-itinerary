bc5907910a68af0ce21dc37205cf9a8f5244f663
pyronasaur <pyronnicholas@gmail.com>
2019-07-24 17:08:45 -0400

    api calls updated from app and added to dd list select, improved login error messages

--- assets/javascript/trip_saver.js
+++ assets/javascript/trip_saver.js
@@ -101,7 +101,10 @@
           $("#departure-input").val(data.startDate);
           $("#return-input").val(data.endDate);          
           $("#tripName-input").val(tripName);
-          $("#location-input").val(data.location);          
+          $("#location-input").val(data.location);
+          
+          $("#weatherColumn").css("visibility","visible");
+          $("#restaurantColumn").css("visibility","visible");
 
           weatherCall();
           restaurantCall();
@@ -110,76 +113,86 @@
 
     
     function weatherCall() {    
-        userCity = $("#location-input").val();
-        
+      userCity = $("#location-input").val();
+      
 
-        //AccuWeather API data
-        var locationKey_queryURL = ("https://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + apiKeyAccuweather + "&q=" + userCity);
+      //AccuWeather API data
+      var locationKey_queryURL = ("https://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + apiKeyAccuweather + "&q=" + userCity);
 
-        //$("#location-input").val("");
+      //$("#location-input").val("");
 
-        $.ajax({
-            url: locationKey_queryURL,
-            method: "GET"
-            }).then(function(response) {
+      $.ajax({
+          url: locationKey_queryURL,
+          method: "GET"
+          }).then(function(response) {
 
-                var locationKey = response[0].Key;
-                //var userCity = response[0].LocalizedName;
-                //$("#userCity").html("<h2>" + userCity + "</h2>");
+              var locationKey = response[0].Key;
+              //var userCity = response[0].LocalizedName;
+              //$("#userCity").html("<h2>" + userCity + "</h2>");
 
-                var currentConditions_queryURL = ("https://dataservice.accuweather.com/currentconditions/v1/" + locationKey + "?apikey=" + apiKeyAccuweather);
+              var currentConditions_queryURL = ("https://dataservice.accuweather.com/currentconditions/v1/" + locationKey + "?apikey=" + apiKeyAccuweather);
 
-                $.ajax({
-                    url: currentConditions_queryURL,
-                    method: "GET"
-                    }).then(function(response) {
-                        var temp = response[0].Temperature.Imperial.Value;
-                        $("#currentTemp").html("<p>" + temp + " F</p>");
-                        var condition = response[0].WeatherText;
-                        $("#currentWeather").html("<p>" + condition + "</p>");
-                        var icon = response[0].WeatherIcon;
-                        //console.log(icon,icon.toString());
-                        if (icon.toString().length < 2){
-                            icon = ("0" + icon);
-                        }
-                        //console.log(icon);
-                        $("#weatherIcon").attr("src","https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/" + icon + "-s.png");
-                        $("#iconDiv").animate({opacity: 1.0}, 2000);
-                    });
-            });
-        }
+              $.ajax({
+                  url: currentConditions_queryURL,
+                  method: "GET"
+                  }).then(function(response) {
+                      var temp = response[0].Temperature.Imperial.Value;
+                      var condition = response[0].WeatherText;
+                      var icon = response[0].WeatherIcon;
+                      $("#weatherData").after().html("<div class='weather'<p>" + temp + " F</p><p>" + condition + "</p>");
+                      //console.log(icon,icon.toString());
+                      if (icon.toString().length < 2){
+                          icon = ("0" + icon);
+                      }
+                      //console.log(icon);
+                      $("#weatherIcon").attr("src","https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/" + icon + "-s.png");
+                      $("#iconDiv").animate({opacity: 1.0}, 2000);
+                  });
+          });
+      }
 
-        function restaurantCall () {
-        
-            //Zomato API data
-            var cityId_queryURL = ("https://developers.zomato.com/api/v2.1/cities?apikey=" + apiKeyZomato + "&q=" + userCity);
+      function restaurantCall () {
+      
+          //Zomato API data
+          var cityId_queryURL = ("https://developers.zomato.com/api/v2.1/cities?apikey=" + apiKeyZomato + "&q=" + userCity);
 
-            $.ajax({
-                url: cityId_queryURL,
-                method: "GET"
-                }).then(function(response) {
-    
-                    var cityId = response.location_suggestions[0].id;
-                    var cityName = response.location_suggestions[0].name;
-                    
-                    $("#cityName").html("<p>" + cityName + "</p>");
-    
-                    var topThreeEats_queryURL = ("https://developers.zomato.com/api/v2.1/search?apikey=" + apiKeyZomato + "&entity_id=" + cityId + "&entity_type=city&count=3&radius=10000&sort=rating&order=desc");
-    
-                    $.ajax({
-                        url: topThreeEats_queryURL,
-                        method: "GET"
-                        }).then(function(response) {
-                            var option1 = response.restaurants[0].restaurant.name;
-                            $("#zomato1").html("<p>" + option1 + "</p>");
-    
-                            var option2 = response.restaurants[1].restaurant.name;
-                            $("#zomato2").html("<p>" + option2 + "</p>");
-    
-                            var option3 = response.restaurants[2].restaurant.name;
-                            $("#zomato3").html("<p>" + option3 + "</p>");
-                        }); 
-                });
-            }
+          $.ajax({
+              url: cityId_queryURL,
+              method: "GET"
+              }).then(function(response) {
+  
+                  var cityId = response.location_suggestions[0].id;
+                  var cityName = response.location_suggestions[0].name;
+                  
+                  $("#cityName").html("<h5>" + cityName + "</h5>");
+  
+                  var topThreeEats_queryURL = ("https://developers.zomato.com/api/v2.1/search?apikey=" + apiKeyZomato + "&entity_id=" + cityId + "&entity_type=city&count=3&radius=10000&sort=rating&order=desc");
+  
+                  $.ajax({
+                      url: topThreeEats_queryURL,
+                      method: "GET"
+                      }).then(function(response) {
+                          var option0Name = response.restaurants[0].restaurant.name;
+                          var option0Address = response.restaurants[0].restaurant.location.address;
+                          var option0Cost = response.restaurants[0].restaurant.average_cost_for_two;
+                          var option0URL = response.restaurants[0].restaurant.menu_url;
+                          $("#zomato1").html("<div class='restaurant'><h5>" + option0Name + "</h5><p><i>" + option0Address + "</i></p><p>Cost for 2 people: <b>$" + option0Cost + "</b></p><p><a href='" + option0URL + "' target='_blank'>menu link</a></p>");
+
+                          var option1Name = response.restaurants[1].restaurant.name;
+                          var option1Address = response.restaurants[1].restaurant.location.address;
+                          var option1Cost = response.restaurants[1].restaurant.average_cost_for_two;
+                          var option1URL = response.restaurants[1].restaurant.menu_url;
+                          $("#zomato2").html("<div class='restaurant'><h5>" + option1Name + "</h5><p><i>" + option1Address + "</i></p><p>Cost for 2 people: <b>$" + option1Cost + "</b></p><p><a href='" + option1URL + "' target='_blank'>menu link</a></p>");
+
+                          var option2Name = response.restaurants[2].restaurant.name;
+                          var option2Address = response.restaurants[2].restaurant.location.address;
+                          var option2Cost = response.restaurants[2].restaurant.average_cost_for_two;
+                          var option2URL = response.restaurants[2].restaurant.menu_url;
+                          $("#zomato3").html("<div class='restaurant'><h5>" + option2Name + "</h5><p><i>" + option2Address + "</i></p><p>Cost for 2 people: <b>$" + option2Cost + "</b></p><p><a href='" + option2URL + "' target='_blank'>menu link</a></p>");
+                          
+
+                      }); 
+              });
+          }
 
 });
